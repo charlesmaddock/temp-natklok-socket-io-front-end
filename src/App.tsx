6fc754824d87eeb284a74078a0470bbcd609c070
Charles <charlesalexander.maddock@gmail.com>
2022-03-28 15:38:12 +0200

    All packages added but not tested.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
 function App() {
-  const socket = io("localhost:3001");
+  const [socket] = useState(io("http://127.0.0.1:3001"));
 
-  const handleConnect = () => {
-    socket.connect();
+  const [roomToJoinId, setRoomToJoinId] = useState("");
+  const [createdRoomId, setCreatedRoomId] = useState("");
+  const [questionId, setQuestionId] = useState("");
+  const [isCorrect, setIsCorrect] = useState<boolean>(false);
+
+  useEffect(() => {
+    socket.on("roomCreated", (data) => {
+      setCreatedRoomId(data.roomId);
+    });
+  }, []);
+
+  const handleCreateRoom = () => {
+    socket.emit("createRoom");
+  };
+
+  const handleJoinRoom = () => {
+    let payload = {
+      roomId: roomToJoinId,
+    };
+    socket.emit("joinRoom", payload);
+  };
+
+  const handleAnsweredQuestion = () => {
+    let payload = {
+      questionId: questionId,
+      isCorrect: isCorrect,
+    };
+    socket.send("answerQuestion", payload);
   };
 
   return (
@@ -17,7 +44,52 @@ function App() {
         height: "100vh",
       }}
     >
-      <button onClick={handleConnect}>Connect</button>
+      <div
+        style={{ background: "rgba(0,0,0,0.09)", borderRadius: 6, padding: 10 }}
+      >
+        <h3 style={{ margin: 0 }}>Temp socket server front-end</h3>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 6,
+          }}
+        >
+          <button onClick={handleCreateRoom}>Create room</button>
+          <p style={{ margin: 0 }}>{createdRoomId}</p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 6,
+          }}
+        >
+          <input
+            placeholder="Room's code"
+            onChange={(e) => setRoomToJoinId(e.target.value)}
+          />
+          <button onClick={handleJoinRoom}>Connect to room</button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 6,
+          }}
+        >
+          <input
+            placeholder="Question's ID"
+            onChange={(e) => setQuestionId(e.target.value)}
+          />
+          <input
+            type="checkbox"
+            onChange={(e) => setIsCorrect(e.target.checked)}
+            checked={isCorrect}
+          />
+          <button onClick={handleAnsweredQuestion}>Answer</button>
+        </div>
+      </div>
     </div>
   );
 }
