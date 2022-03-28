c5c8e9c3503f8adab5bee65675e56219b387e4c5
Charles <charlesalexander.maddock@gmail.com>
2022-03-28 11:36:48 +0200

    connect to socket server

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client";
 
 function App() {
+  const socket = io("localhost:3001");
+
+  const handleConnect = () => {
+    socket.connect();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <button onClick={handleConnect}>Connect</button>
     </div>
   );
 }
